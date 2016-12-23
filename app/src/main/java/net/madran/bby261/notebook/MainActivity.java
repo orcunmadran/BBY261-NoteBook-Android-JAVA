48f8ae6e7a2e795aa7b32d28e2bcb0d38eb0a9db
orcunmadran <orcunmadran@gmail.com>
2016-12-23 12:26:51 +0300

    Not defteri hazır...

--- app/src/main/java/net/madran/bby261/notebook/MainActivity.java
+++ app/src/main/java/net/madran/bby261/notebook/MainActivity.java
@@ -1,13 +1,55 @@
 package net.madran.bby261.notebook;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+    Ayrıntılı bilgiye aşağıdaki adresten ulaşabilirsiniz:
+    https://developer.android.com/training/basics/data-storage/index.html
+    */
+
+    Button kaydetBtn;
+    Button gosterBtn;
+    EditText notesTxt;
+
+    public static final String notes = "notes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kaydetBtn = (Button) findViewById(R.id.kaydetBtn);
+        gosterBtn = (Button) findViewById(R.id.gosterBtn);
+        notesTxt = (EditText)findViewById(R.id.notesTxt);
+
+        kaydetBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                SharedPreferences.Editor editor = getSharedPreferences(notes, MODE_PRIVATE).edit();
+                editor.putString("notesSaved", notesTxt.getText().toString());
+                editor.commit();
+
+            }
+        });
+
+        gosterBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                SharedPreferences prefs = getSharedPreferences(notes, MODE_PRIVATE);
+                Log.i("tag", prefs.getString("notesSaved", ""));
+                notesTxt.setText(prefs.getString("notesSaved", ""));
+
+            }
+        });
+
     }
 }
